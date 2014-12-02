8134adcfbb5026cc6292873afec62eafd473f25c
Floriment <klinakuf@gmail.com>
2014-12-02 16:48:02 +0100

    The first version of the client

---
+++ src/CustomersWindow.java
@@ -0,0 +1,243 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author floriment
+ */
+import com.sun.javafx.scene.control.skin.FXVK;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.ScrollPane;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.List;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+public class CustomersWindow extends JFrame {
+
+    public OmazonClient client;
+
+    /**
+     * The main method showing the GUI.
+     *
+     * @param args either "REST" or "SOAP"
+     */
+    public static void main(String[] args) {
+        CustomersWindow window = new CustomersWindow();
+        window.setVisible(true);
+    }
+
+    // Dimensions of the window.
+    public static final int WINDOW_WIDTH = 500;
+    public static final int WINDOW_HEIGHT = 400;
+
+    public static final String DELETE_STR = "<html><font color=\"red\">Delete</font></html>";
+    public static final String EDIT_STR = "<html><font color=\"green\">Edit</font></html>";
+
+    // The tables for shop and cart
+    JTable customerTable;
+    JLabel priceLabel;
+
+    // The shop and cart contents
+//    ShoppingCart shoppingCart;
+//    Stock stock;
+    // The used service
+//    ShoppingCartService service;
+    // The ID of this client
+    long id;
+
+    public CustomersWindow() {
+        super("Customers - Omazon");
+        client = new OmazonClient();
+        // Exit VM when closing
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.getContentPane().setLayout(new BoxLayout(this.getContentPane(), BoxLayout.X_AXIS));
+        this.setResizable(false);
+        JPanel mainPanel = new JPanel();
+        mainPanel.add(new JLabel("Customers"));
+        // A refresh button
+        JButton refreshShopButton = new JButton("Refresh");
+        ActionListener refreshSopAL = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                updateCustomersView();
+            }
+        };
+        refreshShopButton.addActionListener(refreshSopAL);
+        mainPanel.add(refreshShopButton);
+
+        // Add tables inside of scrollPanes
+        ScrollPane firstScrollPane = new ScrollPane();
+        firstScrollPane.setBounds(0, 0, 450, 300);
+
+        customerTable = new JTable();
+        DefaultTableModel customerTableModel = new DefaultTableModel() {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                if (column == 3 || column == 4 || row == 0) {
+                    return false;
+                }
+                return true;
+            }
+        };
+
+        // Add Click Listener on customerTable
+        customerTable.setModel(customerTableModel);
+        // The columns are: ID, name, price (for 1 product), available amount,
+        // "add to cart" clickable field
+        customerTableModel.setColumnCount(5);
+
+        customerTable.getColumnModel().getColumn(0).setPreferredWidth(30);
+        customerTable.getColumnModel().getColumn(1).setPreferredWidth(100);
+        customerTable.getColumnModel().getColumn(2).setPreferredWidth(120);
+        customerTable.getColumnModel().getColumn(3).setPreferredWidth(60);
+        customerTable.getColumnModel().getColumn(4).setPreferredWidth(60);
+
+        customerTable.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                JTable target = (JTable) e.getSource();
+                int customerTableRow = target.getSelectedRow();
+                int customerTableColumn = target.getSelectedColumn();
+
+                // If we're clicking the "Delete" cell
+                if (customerTableColumn == 4
+                        && target.getModel()
+                        .getValueAt(customerTableRow, customerTableColumn)
+                        .equals(DELETE_STR)) {
+                    int id = (int) target.getModel().getValueAt(
+                            customerTableRow, 0);
+                    client.deleteCustomerById(id);
+                    updateCustomersView();
+                }
+
+                if (customerTableColumn == 3
+                        && target.getModel()
+                        .getValueAt(customerTableRow, customerTableColumn)
+                        .equals(EDIT_STR)) {
+                    int id = (int) target.getModel().getValueAt(
+                            customerTableRow, 0);
+                    String name =  (String) target.getModel().getValueAt(customerTableRow, 1);
+                    String email = (String) target.getModel().getValueAt(customerTableRow, 2);
+                    Customer c = new Customer(id,name,email);
+                    AddCustomerWindow window = new AddCustomerWindow(c);
+//                    CustomersWindow.this.setVisible(false);
+                    window.setVisible(true);
+                    updateCustomersView();
+                }
+            }
+        });
+
+        // The header row
+        customerTableModel.addRow(new Object[]{"<html><b>ID</b></html>",
+            "<html><b>Name</b></html>", "<html><b>Email</b></html>",
+            "<html><b>Edit</b></html>", "<html><b>Delete</b></html>"});
+
+        firstScrollPane.add(customerTable);
+        mainPanel.add(firstScrollPane);
+
+        // Panel to hold clear cart button total price and buy button
+        JPanel bottomPanel = new JPanel();
+        bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.X_AXIS));
+
+        JButton addCustomer = new JButton("Add new Customer!");
+        addCustomer.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                AddCustomerWindow win = new AddCustomerWindow(null);
+//                CustomersWindow.this.setVisible(false);
+                win.setVisible(true);
+            }
+        });
+        bottomPanel.add(addCustomer);
+        // For Space after Button
+        bottomPanel.add(new JLabel("            "));
+        // For displaying Total Price
+        mainPanel.add(bottomPanel);
+        mainPanel.setPreferredSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
+        this.add(mainPanel);
+        this.pack();
+
+        List<Customer> listOfCustomers = client.getCustomers();
+        updateCustomersView();
+        // Get the contents for cart and shop for the first time
+    }
+
+    public void updateCustomersView() {
+        DefaultTableModel customerTableModel = (DefaultTableModel) customerTable
+                .getModel();
+        customerTableModel.setRowCount(1);
+        List<Customer> listOfCustomers = client.getCustomers();
+
+        for (Customer c : listOfCustomers) {
+            customerTableModel.addRow(new Object[]{c.getId(), c.getName(), c.getEmail(), EDIT_STR, DELETE_STR});
+        }
+    }
+
+    public class AddCustomerWindow extends JFrame {
+
+        Customer customer;
+        boolean update = false;
+
+        public AddCustomerWindow(Customer c) {
+            super("Add a new customer");
+
+            if (c == null) {
+                customer = new Customer();
+            } else {
+                customer = c;
+                update = true;
+            }
+            JLabel l = new JLabel();
+            l.setText("	Name :");
+            JTextField name = new JTextField(customer.getName(), 30);
+            JLabel l2 = new JLabel();
+            l2.setText("	Email :");
+            JTextField email = new JTextField(customer.getEmail(), 30);
+            JButton but = new JButton();
+            but.setText(update?"Update":"Add new");
+            add(l);
+            add(name);
+            add(l2);
+            add(email);
+            add(but);
+            but.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    customer.setName(name.getText());
+                    customer.setEmail(email.getText());
+                    if (update) {
+                        client.updateCustomer(customer);
+                    } else {
+                        client.addCustomer(customer);
+
+                    }
+                    AddCustomerWindow.this.setVisible(false);
+                    CustomersWindow.this.setVisible(true);
+                    CustomersWindow.this.updateCustomersView();
+                }
+            });
+            setLayout(new FlowLayout());
+            setSize(400, 400);
+            setVisible(true);
+            setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        }
+
+    }
+}
---
+++ src/OmazonClient.java
@@ -0,0 +1,147 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.reflect.TypeToken;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+import java.lang.reflect.Type;
+import java.util.Collection;
+import java.util.List;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * This class enables us to use the REST protocol with JSON for our clients.
+ *
+ * @author floriment
+ */
+public class OmazonClient {
+
+    // The URI to use
+    public static final String REST_URI = "http://localhost:8080/omazon/";
+
+    // All the resources
+//	private WebResource identifyService;
+//	private WebResource stockService;
+//	private WebResource shoppingCartService;
+//	private WebResource addProductToShoppingCartService;
+//	private WebResource clearShoppingCartService;
+//	private WebResource buyService;
+    private WebResource products;
+    private WebResource customers;
+
+    // A GSON Builder
+    private GsonBuilder gsonBuilder;
+    private Gson gson;
+
+    public static void main(String[] args) {
+        OmazonClient client = new OmazonClient();
+        client.getCustomers();
+        //adding a new customer
+        JsonObject customer = new JsonObject();
+        customer.add("name", new JsonPrimitive("Omar"));
+        customer.add("email", new JsonPrimitive("omar@gmail.com"));
+        client.addCustomer(customer);
+        System.out.println("the updated list");
+        client.getCustomers();
+        JsonObject customerToUpdate = new JsonObject();
+        customerToUpdate.add("id", new JsonPrimitive("1"));
+        customerToUpdate.add("name", new JsonPrimitive("Omar"));
+        customerToUpdate.add("email", new JsonPrimitive("klinakuf@gmail.com"));
+        client.updateCustomer(customerToUpdate);
+        client.getCustomers();
+
+    }
+
+    // Connect using REST
+    public OmazonClient() {
+        ClientConfig config = new DefaultClientConfig();
+        Client client = Client.create(config);
+        WebResource service = client.resource(REST_URI).path("webresources");
+        gson = new Gson();
+
+        products = service.path("com.omazon.entities.products");
+        customers = service.path("com.omazon.entities.customers");
+    }
+
+    /**
+     * Used for simple text methods.
+     *
+     * @param webResource the resource
+     * @return retrieve the text content.
+     */
+    private String getOutputAsText(WebResource webResource) {
+        return webResource.accept(MediaType.TEXT_PLAIN).get(String.class);
+    }
+
+    /**
+     * Used for JSON text methods.
+     *
+     * @param webResource the resource
+     * @return retrieve the JSON text content.
+     */
+    private String getOutputAsJson(WebResource webResource) {
+        return webResource.accept(MediaType.APPLICATION_JSON).get(String.class);
+    }
+
+    /**
+     * Get all customers
+     */
+    public List<Customer> getCustomers() {
+        String response = getOutputAsJson(customers);
+        Type collectionType;
+        collectionType = new TypeToken<List<Customer>>() {
+        }.getType();
+        List<Customer> c = gson.fromJson(response, collectionType);
+        return c;
+    }
+
+    /**
+     * Get a specific customer by id
+     *
+     * @param id
+     */
+    public void getCustomers(int id) {
+        String response = getOutputAsJson(customers.path("" + id));
+        System.out.println(response);
+    }
+
+    /**
+     * Adds a customer
+     *
+     * @param customer
+     */
+    public void addCustomer(JsonObject customer) {
+        customers.type(MediaType.APPLICATION_JSON).post(customer.toString());
+    }
+
+    public void addCustomer(Customer customer) {
+        customers.type(MediaType.APPLICATION_JSON).post(gson.toJson(customer));
+    }
+
+    /**
+     * Updates a customer
+     *
+     * @param customer
+     */
+    public void updateCustomer(JsonObject customer) {
+        customers.path(customer.get("id").getAsString()).type(MediaType.APPLICATION_JSON).put(customer.toString());
+    }
+
+    public void updateCustomer(Customer customer) {
+        customers.path(customer.getId() + "").type(MediaType.APPLICATION_JSON).put(gson.toJson(customer));
+    }
+
+    /**
+     * Use this methot to delete particular customer
+     *
+     * @param customerId
+     */
+    public void deleteCustomerById(int customerId) {
+        customers.path(customerId + "").delete();
+    }
+}
